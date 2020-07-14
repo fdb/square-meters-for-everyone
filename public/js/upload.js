31b8bbea67dc00e9edeb8fddae862ecabb806370
Frederik De Bleser <frederik@debleser.be>
2020-07-14 15:22:16 +0200

    Resize incoming images.

--- public/js/upload.js
+++ public/js/upload.js
@@ -17,7 +17,57 @@ function showMessage(msg) {
   }, 5000);
 }
 
-function onChooseFile(e) {
+// Encode images using https://github.com/eugeneware/jpeg-js
+function getImageDataFromImage2(img) {
+  debugger;
+  const inWidth = img.naturalWidth;
+  const inHeight = img.naturalHeight;
+  const outWidth = 1080;
+  const outHeight = 1920;
+  const factor = Math.min(outWidth / inWidth, outHeight / inHeight);
+  let newWidth = inWidth * factor;
+  let newHeight = inHeight * factor;
+  let ar = 1;
+  if (newWidth < outWidth) ar = outWidth / newWidth;
+  if (Math.abs(ar - 1) < 1e-14 && newHeight < outHeight) ar = outHeight / newHeight;
+  newWidth *= ar;
+  newHeight *= ar;
+
+  // Calculate source rectangle
+  let cw = inWidth / (newWidth / outWidth);
+  let ch = inHeight / (newHeight / outHeight);
+  let cx = (inWidth - cw) * 0.5;
+  let cy = (inHeight - ch) * 0.5;
+
+  const canvas = document.createElement('canvas');
+  canvas.width = outWidth;
+  canvas.height = outHeight;
+  const ctx = canvas.getContext('2d');
+
+  ctx.drawImage(img, cx, cy, cw, ch, 0, 0, outWidth, outHeight);
+  return ctx.getImageData(0, 0, outWidth, outHeight);
+}
+
+function resizeImage(img) {
+  const quality = 85;
+  const imageData = getImageDataFromImage2(img);
+  const jpegData = encode(imageData, quality);
+  return jpegData.data;
+}
+
+async function loadImage(file) {
+  return new Promise((resolve) => {
+    const imgUrl = URL.createObjectURL(file);
+    const img = document.createElement('img');
+    img.addEventListener('load', () => {
+      const buffer = resizeImage(img);
+      resolve(buffer);
+    });
+    img.src = imgUrl;
+  });
+}
+
+async function onChooseFile(e) {
   const file = e.target.files[0];
   const filename = file.name;
   const fileExt = filename.substring(filename.lastIndexOf('.') + 1).toLowerCase();
@@ -25,8 +75,9 @@ function onChooseFile(e) {
     alert('Please only upload images.');
     return;
   }
+  const buffer = await loadImage(file);
   const storageRef = storage.ref(filename);
-  const task = storageRef.put(file);
+  const task = storageRef.put(buffer);
   task.on(
     'state_changed',
     (snapshot) => {
