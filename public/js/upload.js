b8052c299300efe6459e4c14c7100c6ef2677ce4
Frederik De Bleser <frederik@debleser.be>
2020-07-14 11:35:31 +0200

    Upload now works.

--- public/js/upload.js
+++ public/js/upload.js
@@ -1,20 +1,31 @@
 console.log('ohello');
 
+const db = firebase.firestore();
+const storage = firebase.storage();
+
 const IMAGE_EXTENSIONS = ['png', 'jpg', 'jpeg'];
 
 function onClickUploadButton() {
   document.querySelector('#file').click();
 }
 
+function showMessage(msg) {
+  document.querySelector('#progress').innerHTML = '';
+  document.querySelector('#message').textContent = msg;
+  window.setTimeout(() => {
+    document.querySelector('#message').textContent = '';
+  }, 2000);
+}
+
 function onChooseFile(e) {
-  console.log(e);
   const file = e.target.files[0];
   const filename = file.name;
   const fileExt = filename.substring(filename.lastIndexOf('.') + 1).toLowerCase();
   if (!IMAGE_EXTENSIONS.includes(fileExt)) {
     alert('Please only upload images.');
+    return;
   }
-  const storageRef = firebase.storage().ref(filename);
+  const storageRef = storage.ref(filename);
   const task = storageRef.put(file);
   task.on(
     'state_changed',
@@ -23,16 +34,32 @@ function onChooseFile(e) {
       document.querySelector('#progress').innerHTML = `Uploading ${uploadProgress}%`;
     },
     (error) => {
-      this.setState({
-        message: '#ERR(0967)# Error while uploading. Is your file too big?',
-        isUploading: false,
-      });
+      showMessage('Error while uploading. Is your file too big?');
     },
-    (snapshot) => {
+    async (snapshot) => {
       console.log('storageRef', storageRef);
-      this._sendToDB(fileType, storageRef);
+      const imageUrl = await storageRef.getDownloadURL();
+      console.log('imageUrl', imageUrl);
+      createImage(imageUrl);
     }
   );
+}
+
+async function createImage(url) {
+  const obj = {
+    url,
+    created: firebase.firestore.Timestamp.fromDate(new Date()),
+  };
+  db.collection('images')
+    .add(obj)
+    .then((docRef) => {
+      console.log('docRef', docRef);
+      showMessage('Upload complete');
+      document.location.href = '/player';
+    })
+    .catch((error) => {
+      showMessage('Error creating database entry.');
+    });
 }
 
 document.querySelector('.upload').addEventListener('click', onClickUploadButton);
